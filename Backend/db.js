3f38fb90f957da59d0b517f38acc660c4c0e7b1b
Shivtej Gaikwad <91597404+ShivtejG236@users.noreply.github.com>
2025-10-12 11:15:38 +0530

    Create db.js

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json');
+
+const defaultData = { users: [] };
+const adapter = new JSONFile(file);
+const db = new Low(adapter, defaultData);
+
+await db.read();
+db.data ||= defaultData;
+
+export default db;
