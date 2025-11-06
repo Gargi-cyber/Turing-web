1fd5250886676ccbdaf7a92797a7782e6d875b13
Shivtej Gaikwad <91597404+ShivtejG236@users.noreply.github.com>
2025-11-06 23:49:29 +0530

    Update env.js

--- Backend/Config/env.js
+++ Backend/Config/env.js
@@ -7,6 +7,11 @@ export const config = {
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL,
   },
+  github: {
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL,
+  },
   jwt: {
     secret: process.env.JWT_SECRET,
     expiresIn: process.env.JWT_EXPIRES_IN || "7d",
